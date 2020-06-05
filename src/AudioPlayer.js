a227bbf41295b7fa0c844bb7f4721f6cc4a68cc1
ali shirzadeh <shirzadehali973@gmail.com>
2020-06-05 23:55:32 +0430

    Initial Commit

---
+++ src/AudioPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AudioPlayer = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default AudioPlayer;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AudioPlayer from './AudioPlayer';
+
+ReactDOM.render(<AudioPlayer />, document.getElementById('root'));
