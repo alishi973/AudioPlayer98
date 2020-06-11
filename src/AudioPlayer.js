bd8f90e51c27af0766b4f9b198e0d86e36bccdff
ali <shirzadehali973@gmail.com>
2020-06-11 22:23:18 +0430

    Basic Design

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const AudioPlayer = () => {
-  const link =/*  props.link || */ 'https://nex1music.ir/upload/153452501374701878octave-savage-nist.jpg';
+const AudioPlayer = (props) => {
+  const link = props.link || 'https://nex1music.ir/upload/153452501374701878octave-savage-nist.jpg';
+  const simpleMusicLink = 'https://dl.nex1music.ir/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3?time=1591897792&filename=/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3';
   return (
     <div className='window'>
       <div className='title-bar'>
@@ -14,7 +15,21 @@ const AudioPlayer = () => {
       </div>
       <div className='window-body'>
         <div id='audioPlayer'>
-          <img className="audioPlayer__cover" src={link} />
+          <div>
+            <img className='audioPlayer__cover' src={link} />
+          </div>
+          <div className='audioPlayer__controls'>
+            <div className='field-row' style={{ width: '300px' }}>
+              <label htmlFor='range22'>Low</label>
+              <input id='range22' type='range' min='0' max='100' step='1' />
+              <label htmlFor='range23'>High</label>
+            </div>
+            <div className='audioPlayer__controls__buttons'>
+              <button> {'<<'} </button>
+              <button>Play</button>
+              <button>{'>>'}</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
