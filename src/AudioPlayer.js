4b2a7ccc92e739d12653f46e3c2b2510e20f804f
ali <shirzadehali973@gmail.com>
2020-06-14 21:05:43 +0430

    Download With XMLHttpRequest

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const link = 'https://nex1music.ir/upload/153452501374701878octave-savage-nist.jpg';
-// const simpleMusicLink = 'https://www.w3schools.com/html/horse.ogg';
-const simpleMusicLink = 'https://dl.nex1music.ir/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3?time=1591897792&filename=/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3';
+const simpleMusicLink = 'https://www.w3schools.com/html/horse.ogg';
+// const simpleMusicLink = 'https://dl.nex1music.ir/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3?time=1591897792&filename=/1397/05/26/Octave%20-%20Savage%20Nist%20[128].mp3';
 
 const AudioPlayer = (props) => {
   const [music, musicSet] = useState({ audioLink: null, audio: null, isPlayed: false });
   const onPlayClick = () => {
     if (!music.audio) {
-      fetch('https://cors-anywhere.herokuapp.com/' + simpleMusicLink)
+      const fileLink = 'https://cors-anywhere.herokuapp.com/' + simpleMusicLink;
+
+      /* fetch('https://cors-anywhere.herokuapp.com/' + simpleMusicLink)
         .then((res) => res.arrayBuffer())
         .then((blob) => {
           console.log(blob);
@@ -20,7 +22,25 @@ const AudioPlayer = (props) => {
             audioSource.start();
           });
           // musicSet((lastMusic) => ({ ...lastMusic, audio: new AudioBuffer().copyFromChannel(blob) }));
+        }); */
+      const audioContext = new AudioContext();
+      const audioSource = audioContext.createBufferSource();
+
+      const request = new XMLHttpRequest();
+      request.open('GET', fileLink, true);
+      request.responseType = 'arraybuffer';
+      request.send();
+      request.onprogress = (download) => {
+        console.log(download);
+      };
+      request.onload = (file) => {
+        audioContext.decodeAudioData(file.target.response, (res) => {
+          audioSource.buffer = res;
+          audioSource.connect(audioContext.destination);
+          audioSource.start();
         });
+        console.log(file);
+      };
     }
 
     musicSet((lastMusic) => ({ ...lastMusic, isPlayed: !lastMusic.isPlayed }));
